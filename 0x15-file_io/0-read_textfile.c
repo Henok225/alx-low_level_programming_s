9a8d491c8335270b5b67e2df7c98f3e3808b633c
Henok225 <henokzena650@gmail.com>
2023-04-10 14:30:59 -0700

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,18 +10,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char*bufs;
+	char *buf;
 	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(filename,O_RDONLY);
-	if(fd == -1)
-		return(0);
-	bufs=malloc(sizeof(char)*letters);
-	t=read(fd, bufs, letters);
-	w = write(STDOUT_FILENO, bufs, t);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	free(bufs);
+	free(buf);
 	close(fd);
-	return(w);
+	return (w);
+}
+
